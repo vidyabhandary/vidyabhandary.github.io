f0c2d78f6e35c4323d0fe5fe0a9e2cfdcd549691
Vidya <vidya.ga@gmail.com>
2019-07-17 20:31:11 +0530

    Changed quote around font

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-57eed572eeea053ce397.js"
+    "url": "webpack-runtime-44d7a5c1f44bd1c8dfb7.js"
   },
   {
     "url": "app-9f07b51b16c3b9fe5018.js"
@@ -36,10 +36,10 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "538848e8337f02e1c256c2208f839f87"
+    "revision": "6c6e06d838462cb3b93cc734bec82a99"
   },
   {
-    "url": "1-e00347f8bca50fd9301f.js"
+    "url": "1-fc25c57bbdf38941a191.js"
   },
   {
     "url": "component---src-pages-404-js-83e13a2d622ee2b56abe.js"
@@ -49,11 +49,11 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "61e712994c23a52bb7a7caab2dcf8c3c"
+    "revision": "36646630ac29fbc723541c496f438a44"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "60b827e31fc92a265450ed55617f7e0a"
+    "revision": "2491cd5938116d0227c373f721220726"
   },
   {
     "url": "manifest.webmanifest",
